fefeef1d65193944e02c1e4c05f9abaee02be518
Martin Mæland <martinmaeland@outlook.com>
2020-11-09 13:08:42 +0100

    fixed merge issues

--- main/main.ino
+++ main/main.ino
@@ -9,7 +9,6 @@
 #include "motor.h"
 #include "stepper.h"
 #include "pid.h"
-#include "stepper.h"
 
 // Create servo object
 Motor servo;
@@ -17,13 +16,10 @@ Motor servo;
 // Create stepmotor
 StepMotor stepper;
 
-
 void funcA(){
   servo.encoderA();
 }
 
-// Create stepper object
-StepMotor stepper;
 
 void setup() {
   
@@ -44,10 +40,6 @@ void loop() {
   // test
   int potmeter = map(analogRead(A0), 0, 1000, 0, 7);
   PID(potmeter*50, servo);
-
-<<<<<<< HEAD
-=======
-  
   
   if (digitalRead(buttonPin[0]) == 1){
     stepper.control_door = opening;
@@ -58,8 +50,4 @@ void loop() {
     stepper.stepmotor(stepper.control_door); 
   }
 
- 
-  
-  
->>>>>>> b0284df41291816936708b7c1b10a5dc936ffe6a
 }
--- main/stepper.h
+++ main/stepper.h
@@ -1,18 +1,11 @@
 // **** Stepmotor class ****
 
-<<<<<<< HEAD
 #include <dac.h>
-
-class StepMotor{
-=======
-  #include <dac.h>
-
 
  enum door_control{still, opening, closing};
  enum door_state{closed, open, half};
 
  class StepMotor{
->>>>>>> b0284df41291816936708b7c1b10a5dc936ffe6a
   private:
     const int enableA = 69;
     const int enableB = 67;
@@ -29,14 +22,9 @@ class StepMotor{
     StepMotor();
     void cw(void);
     void ccw(void);
-<<<<<<< HEAD
-    void state_of_door(dr_state* door_state);
-    void stepmotor(move_door* state_of_door);
-=======
+
     void state_of_door(door_state state_door);
     void stepmotor(door_control control_door);
->>>>>>> b0284df41291816936708b7c1b10a5dc936ffe6a
-  
 
 };
   
